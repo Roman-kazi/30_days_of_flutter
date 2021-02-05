9195f11aaaacbc1389d25e19429a6cd09fae2088
Roman-kazi <rpgk2016@gmail.com>
2021-02-05 19:25:59 +0530

    Day 3 completed from YT.
    Learned themes, routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:yt_flutter/Home_page.dart';
+import 'package:yt_flutter/pages/Home_page.dart';
+import 'package:yt_flutter/pages/login_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +8,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage()
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.amber),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+
+      },
     );
   }
 }
